ac94438a98ec6d53ee1f55589dc3d67a9abd8cb2
erik savage <erik.k.savage@gmail.com>
2022-02-23 16:22:00 -0800

    adds react router and  NavBar components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import ToDo from './components/Todo/Todo';
 import Settings from './context/settings';
+import NavBar from './components/NavBar/NavBar'
+import SettingsForm from './components/SettingsForm/SettingsForm';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import '@blueprintjs/core/lib/css/blueprint.css';
 
 export default function App() {
   
   return (
-    <>
+    <BrowserRouter>
       <Settings>
-        <ToDo />
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<ToDo />}/>
+          <Route path="/settings" element={<SettingsForm />}/>
+        </Routes>
       </Settings>
-    </>
+    </BrowserRouter>
   ) 
 }
