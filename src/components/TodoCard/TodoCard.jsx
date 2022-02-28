8b30dc37ba96c2d2214ded35facdb0ff82535d2b
erik savage <erik.k.savage@gmail.com>
2022-02-27 16:08:30 -0800

    adds todo id to card

--- src/components/TodoCard/TodoCard.jsx
+++ src/components/TodoCard/TodoCard.jsx
@@ -7,6 +7,7 @@ function TodoCard(props) {
       <p>{props.item.text}</p>
       <p><small>Assigned to: {props.item.assignee}</small></p>
       <p><small>Difficulty: {props.item.difficulty}</small></p>
+      <p><small>ID: {props.item.id}</small></p>
       <div onClick={() => props.toggleComplete(props.item.id)}>Complete: {props.item.complete.toString()}</div>
   </Card>
   )
